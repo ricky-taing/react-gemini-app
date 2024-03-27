5d0057c61944edf36a9430b2611096abb8f09c5c
Ricky102 <ricky.r.taing@gmail.com>
2024-03-27 15:17:32 -0400

    add input and search result box to frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 const App = () => {
+  const [error, setError] = useState("");
+
   return (
     <div className="app">
       <section className="search-section">
         <p>What do you want to know?
           <button className="surprise-btn">Surprise Me</button>
         </p>
+        <div className="input-container">
+          <input
+            value={""}
+            placeholder="When is Christmas...?"
+            onChange={""}
+          />
+          {!error && <button>Ask Me</button>}
+          {error && <button>Clear</button>}
+        </div>
+        {error && <p>{error}</p>}
+        <div className="search-result">
+          <div key={""}>
+            <p className="answer"></p>
+          </div>
+        </div>
       </section>
     </div>
   );

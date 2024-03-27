31ccef49da7d52dfab32697746d7b3348078ed8b
Ricky102 <ricky.r.taing@gmail.com>
2024-03-27 15:52:38 -0400

    add styling to input container, button, search results

--- src/App.js
+++ src/App.js
@@ -5,26 +5,24 @@ const App = () => {
 
   return (
     <div className="app">
-      <section className="search-section">
-        <p>What do you want to know?
-          <button className="surprise-btn">Surprise Me</button>
-        </p>
-        <div className="input-container">
-          <input
-            value={""}
-            placeholder="When is Christmas...?"
-            onChange={""}
-          />
-          {!error && <button>Ask Me</button>}
-          {error && <button>Clear</button>}
+      <p>What do you want to know?
+        <button className="surprise-btn">Surprise Me</button>
+      </p>
+      <div className="input-container">
+        <input
+          value={""}
+          placeholder="When is Christmas...?"
+          onChange={""}
+        />
+        {!error && <button>Ask Me</button>}
+        {error && <button>Clear</button>}
+      </div>
+      {error && <p>{error}</p>}
+      <div className="search-result">
+        <div key={""}>
+          <p className="answer"></p>
         </div>
-        {error && <p>{error}</p>}
-        <div className="search-result">
-          <div key={""}>
-            <p className="answer"></p>
-          </div>
-        </div>
-      </section>
+      </div>
     </div>
   );
 }

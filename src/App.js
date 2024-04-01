f5b46c81ee0d392511cf5de4dc08326dd23196a3
Ricky102 <ricky.r.taing@gmail.com>
2024-04-01 14:52:53 -0400

    generate random question when click 'Surprise Me' button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import { useState } from "react";
 
 const App = () => {
-  const [error, setError] = useState("");
+  const [ value, setValue ] = useState("");
+  const [ error, setError ] = useState("");
+  const [ chatHistory, setChatHistory ] = useState([]);
+
+  const surpriseOptions = [
+    'Who won the latest Nobel Peace Prize?',
+    'Where does pizza come from?',
+    'How do you make a BLT sandwich?'
+  ];
+
+  const surprise = () => {
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)];
+    setValue(randomValue); 
+  }
 
   return (
     <div className="app">
       <p>What do you want to know?
-        <button className="surprise-btn">Surprise Me</button>
+        <button className="surprise-btn" onClick={surprise} disabled={!chatHistory}>Surprise Me</button>
       </p>
       <div className="input-container">
         <input
-          value={""}
+          value={value}
           placeholder="When is Christmas...?"
-          onChange={""}
+          onChange={(e) => setValue(e.target.value)}
         />
         {!error && <button>Ask Me</button>}
         {error && <button>Clear</button>}
